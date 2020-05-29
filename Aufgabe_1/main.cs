0bc59f185a4eb7de56c6676d78fcd6a6d70134da
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-05-29 21:43:39 +0200

    First version of Aufgabe 1

---
+++ Aufgabe_1/main.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SoftwareentwicklungTUBAF
+{
+  class ClassA {
+  }
+  struct StructA {
+  }
+  interface InterfaceA {
+  }
+  abstract class AbstractClass {
+  }
+  static class StaticClass {
+  }
+  sealed partial class ClassB{
+  }
+  enum EnumType{
+    Apple,
+    Orange,
+    Grape
+  }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}

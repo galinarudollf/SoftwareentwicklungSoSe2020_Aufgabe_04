d23c57a32eb5cbd760a6881148fb6dd07b1b3e40
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-05-30 00:33:07 +0200

    Add new class structure

--- Aufgabe_1/main.cs
+++ Aufgabe_1/main.cs
@@ -3,25 +3,10 @@ using System;
 namespace SoftwareentwicklungTUBAF
 {
   class ClassA {
-  }
-  struct StructA {
-  }
-  interface InterfaceA {
-  }
-  abstract class AbstractClass {
-  }
-  static class StaticClass {
-  }
-  sealed partial class ClassB{
-  }
-  enum EnumType{
-    Apple,
-    Orange,
-    Grape
+      private int justAtest;
   }
 
-
-    class Program
+  class Program
     {
         static void Main(string[] args)
         {

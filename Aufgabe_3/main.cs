72b310131a7eaeabafd3cfc2d0b9fcaaad660f95
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2020-06-01 11:39:21 +0200

    Create main.cs

---
+++ Aufgabe_3/main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Data;
+
+namespace InterfaceDemo
+{
+    // Interface SmartPhone    
+    interface iSmartPhone
+    {
+        protected string Phonenumber { get; set; }
+        // Hier fehlt noch einiges ...
+        public string getOS();
+    }
+
+    // Hier starten Sie ...
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplePhone MyApple = new ApplePhone("0123-2324");
+            SamsungPhone MySamsung = new SamsungPhone("0049-3242-234");
+        }
+    }
+}
